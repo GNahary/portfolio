534bc3b854394aec4834ce37b5cd0273e247785c
GNahary <gnahary@gmail.com>
2024-09-25 09:47:24 +0300

    Adding option to select which stock to present

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,24 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { TradeData } from '../../types';
 import { StockChartComponent } from "../stock-chart/stock-chart.component";
 import { StockCacheService } from '../stock-cache.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [StockChartComponent],
+  imports: [StockChartComponent, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
 
-  stockSymbol = 'TSLA';
   data: TradeData | null = null;
   isLoading: boolean = false;
 
   constructor(private stockCacheService: StockCacheService) { }
 
-  ngOnInit(): void {
-    this.stockCacheService.loadDataFromCache(this.stockSymbol).subscribe(returnedData => this.data = returnedData);
+  ngOnInit(): void { }
+
+  searchForStock(stockSymbol: string) {
+    this.stockCacheService.loadDataFromCache(stockSymbol).subscribe(returnedData => this.data = returnedData);
   }
 
 }
